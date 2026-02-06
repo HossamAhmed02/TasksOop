d6a6e4539981a818251454074e32475d5c789d13
Hossam Ahmed <hossam.a7med02@gmail.com>
2026-02-06 23:47:37 +0200

    Add files via upload

---
+++ Bank.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Bank
+    {
+        const string BankCode = "BNK001";
+        readonly DateTime CreatedDate;
+        private int _accountNumber;
+        private string _fullName;
+        private double _nationalID;
+        private string _phoneNumber;
+        private string _address;
+        private float _balance;
+
+        public string FullName 
+        {
+            get { return _fullName; } 
+            set {
+                if (string.IsNullOrWhiteSpace(value)){
+                    throw new Exception("Please Enter Your Name. Try Again..");
+                }
+                else
+                {
+                    _fullName = value;
+                }
+            }
+        }
+        public double NationalID 
+        { 
+            get { return _nationalID; }
+            set{
+                if(value.ToString().Length == 14)
+                {
+                    _nationalID = value;
+                }
+                else { throw new Exception("Please Enter a Valid National ID."); }
+            } 
+        }
+        public string PhoneNumber 
+        { 
+            get { return _phoneNumber; }
+            set {
+                if (value.Length == 11 && value.StartsWith("01"))
+                {
+                    _phoneNumber = value;
+                }
+                else
+                {
+                    throw new Exception("Please Enter a Valid Phone Number.");
+                }
+            }
+        }
+        public float Balance
+        {
+            get { return _balance; }
+            set
+            {
+                if(value > 0)
+                {
+                    _balance = value;
+                }
+                else { throw new Exception("Enter Valid Balance."); }
+            }
+        }
+        public string Address 
+        { 
+            get { return _address; }
+            set { _address = value; }
+        }
+        public Bank()
+        {
+            throw new Exception("This is Default Constructor");
+        }
+        public Bank(string name , double ID , string phoneNumber , float balance)
+        {
+            _fullName = name;
+            _nationalID = ID;
+            _phoneNumber = phoneNumber;
+            _balance = balance;
+        }
+        public Bank(string name, double ID, string phoneNumber ) : this (name , ID , phoneNumber , 0)
+        {
+            _fullName = name;
+            _nationalID = ID;
+            _phoneNumber = phoneNumber;
+        }
+        public void ShowAccountDetails()
+        {
+            Console.WriteLine(_fullName + "  " + _nationalID + "  " + _phoneNumber + "  " + _balance);
+            Console.WriteLine("----------");
+        }
+        public bool IsValidNationalID()
+        {
+            if (_nationalID.ToString().Length == 14)
+            {
+                Console.WriteLine("Validation ID True");
+                return true;
+            }
+            else
+            {
+                throw new Exception("Please Enter a Valid National ID.");
+                return false;
+            }
+        }
+        public bool IsValidPhoneNumber()
+        {
+            if (_phoneNumber.Length == 11 && _phoneNumber.StartsWith("01"))
+            {
+                Console.WriteLine("Validation Phone Number true");
+                return true;
+            }
+            else
+            {
+                throw new Exception("Please Enter a Valid Phone Number.");
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank a = new Bank("Hossam",12345678901234,"01123456789");
+            Bank b = new Bank("Ahmed", 99999999999999, "01223456789", 1000);
+            a.ShowAccountDetails();
+            b.ShowAccountDetails();
+
+        }
+    }
+}
